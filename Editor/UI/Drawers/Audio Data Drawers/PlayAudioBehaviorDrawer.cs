fc4a0abd22b9ff25318bb2171aa556c53700bcee
Simon Lerch <simon.lerch@innoactive.de>
2020-04-27 13:20:44 +0200

    Adapted naming of play audio behavior

--- Editor/UI/Drawers/Audio Data Drawers/PlayAudioBehaviorDrawer.cs
+++ Editor/UI/Drawers/Audio Data Drawers/PlayAudioBehaviorDrawer.cs
@@ -22,12 +22,7 @@ namespace Innoactive.CreatorEditor.Core.UI.Drawers
                 return base.GetTypeNameLabel(value, declaredType);
             }
 
-            if (behavior.AudioData is ResourceAudio)
-            {
-                return new GUIContent("Play Audio File");
-            }
-
-            return base.GetTypeNameLabel(value, declaredType);
+            return new GUIContent("Play " + behavior.AudioData.Name);
         }
     }
 }
